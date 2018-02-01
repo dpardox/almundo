406c9661b225911d941ec9a0e9fb1ee4ba3a378f
Steven Pardo <dspardo6@gmail.com>
2018-02-01 01:19:42 -0500

    busquda por nombre

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -12,7 +13,7 @@ import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent, FilterComponent, CardComponent, HomeComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [BrowserModule, HttpClientModule, FormsModule],
   providers: [HotelService],
   bootstrap: [AppComponent]
 })
--- src/app/filter/filter.component.ts
+++ src/app/filter/filter.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HotelService } from '../../services/hotel/hotel.service';
 
 @Component({
   selector: 'app-filter',
@@ -7,12 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FilterComponent implements OnInit {
   public expand = false;
+  public name = '';
 
-  constructor() {}
+  constructor(private hotel: HotelService) {}
 
   ngOnInit() {}
 
   public open() {
     this.expand = !this.expand;
+  }
+
+  public search() {
+    console.log(this.name);
+    this.hotel.search(this.name);
+    this.expand = false;
   }
 }
